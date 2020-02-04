b3d9c4672ba2086eb68323f11c50005444f6f7ae
Robert Schneider <robmschn@gmail.com>
2020-02-04 10:00:22 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+ENV = Environment(loader=FileSystemLoader('.'))
+
+baseline = ENV.get_template("template.txt")
+
+with open("basic-data.yml") as y:
+ host_obj = yaml.load(y)
+ f = open('config.txt', 'w')
+ config = baseline.render(host_obj)
+ f.write(config)
+ f.close
